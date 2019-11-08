bce6e4640cf05d54410c00582e9a52d60fa65da0
adancondori <adan.condoric@gmail.com>
2019-11-08 10:49:31 -0400

    add example Game Marbles

---
+++ 002 Swift-GameMarbles/Project/OMGMarbles/GameViewController.swift
@@ -0,0 +1,48 @@
+//
+//  AppDelegate.swift
+//  GameMarbles
+//
+//  Created by Adan Condori on 05/11/2019.
+//  Copyright © 2019 adancondoric@gmail with Swift. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                scene.scaleMode = .resizeFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
